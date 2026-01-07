099961591f1e6fc2e2faec3e4210579067825f5c
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2026-01-07 18:22:38 +0000

    Remove createEventQueue patch
    precompiled binary now. if you wish to recompile this lib yourself, pick before this

--- apply.sh
+++ apply.sh
@@ -4,9 +4,6 @@ cd frameworks/base
 git am --signoff < ~/7420_patches-lineage-19.1/frameworks_base/0002-Some-services-should-just-stfu.patch
 git am --signoff < ~/7420_patches-lineage-19.1/frameworks_base/0003-Stop-davey-messages-when-using-latch_unsignalled.patch
 cd ../../
-cd frameworks/native
-git am --signoff < ~/7420_patches-lineage-19.1/frameworks_native/0001-Add-back-pre-S-createEventQueue-function.patch
-cd ../../
 cd hardware/libhardware
 git am --signoff < ~/7420_patches-lineage-19.1/hardware_libhardware/0001-keystore-hackup.patch
 cd ../../
