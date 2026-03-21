11a41fea98c76d093cc2bfddc09535a592f79089
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2026-03-21 12:58:42 +0000

    art: remove cache segfault patches
    This was added to circumvent checks in the OS for kernel versions above 3.12. It effectively halved performance in many apps.
    
    We spoof the kernel version now for the appropriate processes, so this can be safely removed.

--- apply.sh
+++ apply.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-cd art
-echo "Applying patches to art..."
-git am --signoff < ~/7420_patches-lineage-18.1/android_art/0001-Ignore-checking-for-cache-segfaults-based-on-kernel-.patch
-cd ../
 cd packages/apps/Nfc
 echo "Applying patches to Packages/apps/Nfc..."
 git am --signoff < ~/7420_patches-lineage-18.1/packages_apps_Nfc/Add_sysprop_to_prevent_FW_download.patch
