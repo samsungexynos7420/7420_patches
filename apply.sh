797bbc3d597eb20dfd6f337fc7aace3178afcf0c
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2026-03-28 12:23:27 +0000

    Add nfc patch

--- apply.sh
+++ apply.sh
@@ -13,3 +13,7 @@ cd ../../
 cd system/security
 git am --signoff < ~/7420_patches-lineage-19.1/system_security/0001-keystore-hackup.patch
 cd ../../
+cd packages/apps/Nfc
+echo "Applying patches to Packages/apps/Nfc..."
+git am --signoff < ~/7420_patches-lineage-19.1/packages_apps_Nfc/Add_sysprop_to_prevent_FW_download.patch
+cd ../../../
