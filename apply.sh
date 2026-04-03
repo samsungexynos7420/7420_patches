79044c638e9458b85712df0c74ef89d970f55d19
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2026-04-03 16:48:33 +0100

    Add nfc fw patch

--- apply.sh
+++ apply.sh
@@ -18,3 +18,7 @@ cd frameworks/base
 echo "Applying patches to frameworks/base"
 git am --signoff < ~/7420_patches-lineage-20.0/frameworks_base/0001-Revert-fp-always-on-changes.patch
 cd ../../
+cd packages/apps/Nfc
+echo "Applying patches to Packages/apps/Nfc..."
+git am --signoff < ~/7420_patches-lineage-20.0/packages_apps_Nfc/Add_sysprop_to_prevent_FW_download.patch
+cd ../../../
