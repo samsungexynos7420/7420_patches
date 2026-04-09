8fb67ef1df072518b3e779106d005205ccdf54a7
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2026-04-09 14:42:19 +0100

    Do not apply frameworks/native patches
    it seems like this is not necessary for now.

--- apply.sh
+++ apply.sh
@@ -8,12 +8,6 @@ cd system/security
 echo "Applying patches to security"
 git am --signoff < ~/7420_patches-lineage-20.0/system_security/0001-keystore-hackup.patch
 cd ../../
-cd frameworks/native
-echo "Applying patches to frameworks/native"
-git am --signoff < ~/7420_patches-lineage-20.0/frameworks_native/0001-Disable-gpu-service.patch
-git am --signoff < ~/7420_patches-lineage-20.0/frameworks_native/0001-Revert-Remove-obsolete-debug-option.patch
-git am --signoff < ~/7420_patches-lineage-20.0/frameworks_native/0002-Add-back-pre-S-createEventQueue-function.patch
-cd ../../
 cd frameworks/base
 echo "Applying patches to frameworks/base"
 git am --signoff < ~/7420_patches-lineage-20.0/frameworks_base/0001-Revert-fp-always-on-changes.patch
