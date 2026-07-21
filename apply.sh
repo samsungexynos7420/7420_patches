73afece7fd6c245940affac1a453ea64a20d8514
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2026-07-21 15:06:25 +0100

    Remove NFC Patches
    this is patched in the vendor implementation now

--- apply.sh
+++ apply.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cd packages/apps/Nfc
-echo "Applying patches to Packages/apps/Nfc..."
-git am --signoff < ~/7420_patches-lineage-18.1/packages_apps_Nfc/Add_sysprop_to_prevent_FW_download.patch
-cd ../../../
-
-
 echo "\n Applying batteryextender-eleven patches..."
 
 cd device/lineage/sepolicy
